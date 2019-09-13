3356d7f7b94cd0925cacbe2b29945a8fbdc64c68
UselesssCat <arie.cbpro@gmail.com>
2019-09-13 18:30:48 -0300

    Add fa icons to search bar

--- src/components/dashboard/topbar.js
+++ src/components/dashboard/topbar.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch, faBars);
+
 export function TopbarSearch(props) {
     return (
         <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
@@ -7,7 +14,7 @@ export function TopbarSearch(props) {
                 <input type="text" className="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
                 <div className="input-group-append">
                     <button className="btn btn-primary" type="button">
-                        <i className="fas fa-search fa-sm"></i>
+                        <FontAwesomeIcon icon="search" size="sm" />
                     </button>
                 </div>
             </div>
@@ -19,7 +26,7 @@ export function Topbar(props) {
     return (
         <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
             <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
-                <i className="fa fa-bars"></i>
+                <FontAwesomeIcon icon="bars" />
             </button>
             {props.children}
         </nav>
