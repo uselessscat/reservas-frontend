2353c08874060bf0506d1766ac17f954c7ba2f45
UselesssCat <arie.cbpro@gmail.com>
2019-09-24 09:04:42 -0300

    Fixes table style

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -4,48 +4,47 @@ import { Table as ReactstrapTable } from 'reactstrap';
 export default function Table(props) {
     return (
         <div>
-            <div className='row'>
+            <div className='row '>
                 <div className='col-sm-12 col-md-6'>
-                    <div className='dataTables_length' id='dataTable_length'>
-                        <label>Show
-                            <select name='dataTable_length' aria-controls='dataTable' className='custom-select custom-select-sm form-control form-control-sm'>
-                                <option value='10'>10</option>
-                                <option value='25'>25</option>
-                                <option value='50'>50</option>
-                                <option value='100'>100</option>
-                            </select> entries
-                        </label>
-                    </div>
+                    <label className='d-inline-flex'>
+                        <span className='mr-2'>Show </span>
+                        <select className='custom-select custom-select-sm form-control form-control-sm'>
+                            <option value='10'>10</option>
+                            <option value='25'>25</option>
+                            <option value='50'>50</option>
+                            <option value='100'>100</option>
+                        </select>
+                        <span className='ml-2'>entries</span>
+                    </label>
                 </div>
-                <div className='col-sm-12 col-md-6'>
-                    <div id='dataTable_filter' className='dataTables_filter'>
-                        <label>Search: <input type='search' className='form-control form-control-sm' placeholder='' aria-controls='dataTable' />
-                        </label>
-                    </div>
+                <div className='col-sm-12 col-md-6 text-right'>
+                    <label className='d-inline-flex'>
+                        <span className='mr-2'>Search:</span>
+                        <input type='search' className='form-control form-control-sm'/>
+                    </label>
                 </div>
             </div>
-
             <div className='row'>
-                <ReactstrapTable hover striped bordered>
-                    {props.children}
-                </ReactstrapTable>
+                <div className='col-12'>
+                    <ReactstrapTable hover striped bordered>
+                        {props.children}
+                    </ReactstrapTable>
+                </div>
             </div>
             <div className="row">
                 <div className="col-sm-12 col-md-5">
                     <div className="dataTables_info" id="dataTable_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
                 </div>
                 <div className="col-sm-12 col-md-7">
-                    <div className="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
-                        <ul className="pagination">
-                            {['Previous', 1, 2, 3, 4, 5, 6, 'Next'].map((item, index) => {
-                                return (
-                                    <li key={index} className="paginate_button page-item previous" id="dataTable_previous">
-                                        <a href="#" aria-controls="dataTable" data-dt-idx="0" tabIndex="0" className="page-link">{item}</a>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </div>
+                    <ul className="pagination justify-content-end">
+                        {['Previous', 1, 2, 3, 4, 5, 6, 'Next'].map((item, index) => {
+                            return (
+                                <li key={index} className="paginate_button page-item previous">
+                                    <a href="#" data-dt-idx="0" tabIndex="0" className="page-link">{item}</a>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
         </div>
