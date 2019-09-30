81741b049c92e32ea98c0ecf15500dd12ae6c3fd
Hugo Paez <hapj@outlook.cl>
2019-09-30 11:57:30 -0300

    Add routing for role

--- src/routing/module-router.js
+++ src/routing/module-router.js
@@ -12,10 +12,12 @@ import Dashboard from '../views/dashboard/dashboard';
 import Sidebar from '../views/dashboard/sidebar';
 import Topbar from '../views/dashboard/topbar';
 import Footer from '../views/dashboard/footer';
+
 import Persons from '../views/persons/persons';
 import NewPersons from '../views/persons/persons-new';
 import BranchOffices from '../views/branch-offices/branch-offices';
 import NewBranchOffices from '../views/branch-offices/branch-offices-new';
+import Roles from '../views/roles/roles';
 
 const DashboardRoutes = () => (
     <Switch>
@@ -27,6 +29,8 @@ const DashboardRoutes = () => (
 
         <PrivateRoute exact path={routes.branchOffices.path} component={BranchOffices} />
         <PrivateRoute path={routes.branchOffices.path} component={NewBranchOffices} />
+
+        <PrivateRoute exact path={routes.roles.path} component={Roles} />
 
         <PrivateRoute component={Error404} />
     </Switch >
