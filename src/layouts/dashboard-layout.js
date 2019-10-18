a2284c5d9b039e06cd6988f12c60102db1eda612
UselesssCat <arie.cbpro@gmail.com>
2019-10-18 09:20:15 -0300

    Update dashboard

--- src/layouts/dashboard-layout.js
+++ src/layouts/dashboard-layout.js
@@ -1,36 +1,213 @@
 import React from 'react';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { AppBar, Toolbar, IconButton, Icon, Typography, Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
 
+import { withStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, IconButton, Icon, Typography, Button, Drawer, Container, List, Divider, Grid, Badge, ListItem, ListItemIcon, ListItemText, ListSubheader, CssBaseline } from '@material-ui/core';
+
+import Copyright from '../components/dashboard/copyright';
+
+const drawerWidth = 240;
 const styles = theme => ({
     root: {
-        flexGrow: 1,
+        display: 'flex',
+    },
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
     },
     menuButton: {
-        marginRight: 'theme.spacing(2)',
+        marginRight: 36,
+    },
+    menuButtonHidden: {
+        display: 'none',
     },
     title: {
         flexGrow: 1,
     },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+        },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
 });
 
 class DashboardLayout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: true
+        };
+    }
+
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
+
     render() {
         const { classes } = this.props;
 
         return (
             <div>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <CssBaseline />
+                <AppBar position="absolute" className={clsx(classes.appBar, this.state.open && classes.appBarShift)}>
+                    <Toolbar className={classes.toolbar}>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={clsx(classes.menuButton, this.state.open && classes.menuButtonHidden)}>
                             <Icon>menu</Icon>
                         </IconButton>
-                        <Typography variant="h6" className={classes.title}>News</Typography>
-                        <Button color="inherit">Login</Button>
+                        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>Dashboard</Typography>
+                        <IconButton color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <Icon>notifications</Icon>
+                            </Badge>
+                        </IconButton>
                     </Toolbar>
                 </AppBar>
-                {this.props.children}
+
+                <Drawer variant='permanent'
+                    classes={{
+                        paper: clsx(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+                    }}
+                    open={this.state.open}>
+                    <div className={classes.toolbarIcon}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            <Icon>chevron_left</Icon>
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        <div>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>dashboard</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Dashboard" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>shoppingcart</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Orders" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>people</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Customers" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>bar_chart</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Reports" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>layers</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Integrations" />
+                            </ListItem>
+                        </div>
+                    </List>
+                    <Divider />
+                    <List>
+                        <div>
+                            <ListSubheader inset>Saved reports</ListSubheader>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>assignment</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Current month" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>assignment</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Last quarter" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Icon>assignment</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Year-end sale" />
+                            </ListItem>
+                        </div>
+                    </List>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.appBarSpacer} />
+                    <Container maxWidth="lg" className={classes.container}>
+                        <Grid container spacing={3}>
+                            {this.props.children}
+                        </Grid>
+                    </Container>
+                    <Copyright />
+                </main>
             </div>
         )
     }
@@ -41,22 +218,3 @@ DashboardLayout.propTypes = {
 };
 
 export default withStyles(styles)(DashboardLayout)
-/*
-
-<SidebarList>
-                    {<this.props.sidebarContent />}
-                </SidebarList>
-                <div id="content-wrapper" className="d-flex flex-column">
-                    <div id="content">
-                        <Topbar>
-                            {<this.props.topbarContent />}
-                        </Topbar>
-                        <div className="container-fluid">
-                            {this.props.children}
-                        </div>
-                    </div>
-                    <Footer>
-                        {<this.props.footerContent />}
-                    </Footer>
-                </div>
-*/
--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Dashboard(props) {
     return (
-        <h1 className='h3 mb-4 text-gray-800'>Dashboard</h1>
+        <div>
+            Dashboard
+        </div>
     )
 }
